d8ff545a9a612565e333e8b3a8e585547db5ea26
contact@studiologeek.com <gtl92@MacBookProGTLM2.local>
2025-08-13 17:06:45 +0200

    Nettoyage du dépôt avec .gitignore

--- frontend/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.D3tDrJxW.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CqDdcZic.js","_app/immutable/chunks/PWCouxKx.js","_app/immutable/chunks/CmJWyg9Y.js"];
-export const stylesheets = [];
-export const fonts = [];
--- frontend/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.6j9446X9.js","_app/immutable/chunks/CekVB0OK.js","_app/immutable/chunks/PWCouxKx.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CqDdcZic.js"];
-export const stylesheets = ["_app/immutable/assets/2.CF0TUbnF.css"];
-export const fonts = [];
